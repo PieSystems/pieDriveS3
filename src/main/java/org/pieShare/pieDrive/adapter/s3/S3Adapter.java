8d34bbeae3bef5c1c4b2afe1195a56fc0a987ba2
Paul Mueller <e1027426@student.tuwien.ac.at>
2015-11-20 12:29:05 +0100

    S3 adapter, tests missing

---
+++ src/main/java/org/pieShare/pieDrive/adapter/s3/S3Adapter.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pieShare.pieDrive.adapter.s3;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.pieShare.pieDrive.adapter.api.Adaptor;
+import org.pieShare.pieDrive.adapter.model.PieDriveFile;
+
+
+public class S3Adapter implements Adaptor{
+    private AmazonS3Client s3client = new AmazonS3Client();
+	private String bucketName = "g4t2";
+
+	@Override
+	public void delete(PieDriveFile file) {
+		s3client.deleteObject(new DeleteObjectRequest(bucketName, file.getUuid()));
+	}
+
+	@Override
+	public void upload(PieDriveFile file, InputStream stream) {
+		s3client.putObject(new PutObjectRequest(bucketName, file.getUuid(), stream, null));
+	}
+
+	@Override
+	public void download(PieDriveFile file, OutputStream stream) {
+		byte[] buf = new byte[1024];
+		int count = 0;
+		
+		S3Object object = s3client.getObject(new GetObjectRequest(bucketName, file.getUuid()));
+		InputStream objectData = object.getObjectContent();
+		
+		try{
+			while((count = objectData.read(buf)) != -1) {
+			   if(Thread.interrupted() )
+			   {
+				   //throw new InterruptedException();
+			   }
+
+			   stream.write(buf, 0, count);
+			}
+
+			stream.close();
+			objectData.close();
+			
+		} catch(IOException e){
+			//TODO
+		}
+	}
+    
+	
+
+}
---
+++ src/test/java/S3AdapterTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.Test; 
+
+public class S3AdapterTest {
+	
+	@Test
+	public void uploadTest(){
+		
+	}
+}
