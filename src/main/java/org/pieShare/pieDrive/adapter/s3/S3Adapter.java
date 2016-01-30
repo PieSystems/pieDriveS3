b5904dc4cc7e5fc53567216db3f850f430f0f1c7
Paul Mueller <e1027426@student.tuwien.ac.at>
2016-01-30 18:23:29 +0100

    custom config directory

--- src/main/java/org/pieShare/pieDrive/adapter/s3/S3Adapter.java
+++ src/main/java/org/pieShare/pieDrive/adapter/s3/S3Adapter.java
@@ -8,17 +8,18 @@ package org.pieShare.pieDrive.adapter.s3;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.PropertiesFileCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.Region;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.amazonaws.services.s3.model.Region;
-import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -33,7 +34,16 @@ public class S3Adapter implements Adaptor{
 	private final String bucketName = "g4t2aic2015";
 	
 	public S3Adapter(){
-		this.s3client = new AmazonS3Client();
+		loadConf();
+	}
+	
+	private void loadConf(){
+		String path = System.getProperty("user.home");
+		File pieDrive = new File(path, ".pieDrive");
+		File awsFile = new File(pieDrive, "aws");
+		
+		AWSCredentialsProvider provider = new ProfileCredentialsProvider(awsFile.getAbsolutePath(), "default");
+		this.s3client = new AmazonS3Client(provider);
 
 		if(!s3client.doesBucketExist(bucketName)){
 			s3client.createBucket(bucketName);
